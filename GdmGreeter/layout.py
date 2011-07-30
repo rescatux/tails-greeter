75be0ebd7eb42fe1ec6dccfc1216e5a52dedc7a4
Max S <govnototalitarizm@gmail.com>
2011-07-30 16:04:59 +0800

    add layout variant selection stub

--- GdmGreeter/layout.py
+++ GdmGreeter/layout.py
@@ -19,17 +19,32 @@
 Greeter program for GDM using gtk (nothing else works)
 """
 
-import logging
+import logging, gtk, xklavier
 
 from GdmGreeter.language import TranslatableWindow
+
+def print_variant(c_reg, item):
+    print ('\t %s (%s)' % (item.get_description(), item.get_name()))
 
 class LayoutWindow(TranslatableWindow):
     """Display layout selection window"""
     name = 'layout'
     primary = False
+    configreg = None
+    layout = 'us'    
 
     def __init__(self, *args, **kwargs):
         TranslatableWindow.__init__(self, *args, **kwargs)
+        self.configreg = xklavier.ConfigRegistry(xklavier.Engine(gtk.gdk.display_get_default()))
+
+    def filter_layout(self, c_reg, item):
+        if item.get_name() == self.layout:
+            print ('\n%s (%s)' % (item.get_description(), item.get_name()))
+            c_reg.foreach_layout_variant(item.get_name(), print_variant)
+
+    def get_variants(self):
+        self.configreg.load(False)
+        return self.configreg.foreach_layout(self.filter_layout)
 
     def button_clicked(self, widget):
         """Signal event to move to next widget"""
