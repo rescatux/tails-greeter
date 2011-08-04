d36d484e69b7cb0c226c3afdec0c2257a43402a6
Max S <govnototalitarizm@gmail.com>
2011-08-05 01:55:52 +0800

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from DistUtilsExtra.command import *
 from fnmatch import fnmatch
 
 __appname__ = 'tails-greeter'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 import os, platform
 
 # remove MANIFEST. distutils doesn't properly update it
