316c1f41f6b022a25a96d0adc2089ca823208850
Tails developers <tails@boum.org>
2012-02-25 16:09:51 +0100

    Releasing tails-greeter 0.2.0
    This is a milestone after quite a lot of important changes were made and
    stabilized, and before the persistence support integration work starts.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from DistUtilsExtra.command import *
 from fnmatch import fnmatch
 
 __appname__ = 'tails-greeter'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 import os, platform
 
 # remove MANIFEST. distutils doesn't properly update it
