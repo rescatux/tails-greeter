rescatux/tails-greeter

commit c45fc6032506e5f71d41f01dd5c15e495128d5e8

Remove old D-Bus code, rename method.

community-greeter.py
@@ -71,7 +71,7 @@ class CommunityGreeterApp():
         self.translated = False
         self.gdmclient = tailsgreeter.gdmclient.GdmClient(
             server_ready_cb=self.server_ready,
-            session_opened_cb = self.FinishProcess
+            session_opened_cb = self.close_app
         )
         self.persistence = tailsgreeter.persistence.PersistenceSettings()
         self._loaded_windows = []
@@ -113,22 +113,7 @@ class CommunityGreeterApp():
         self.ready = True
         logging.warn("greeter is ready.")
 
-    def SelectLanguage(self, lang):
-        """The user wants to change languages"""
-        # Translate all windows in the login screen
-        logging.debug("Translating interface to %s" % lang)
-        self.translate_to(lang)
-        # XXX: Apply chosen language for the session to be started
-        # from gi.repository import AccountsService
-        # import locale
-        # import tailsgreeter.config
-        # usermanager = AccountsService.UserManager()
-        # user = usermanager.get_user(tailsgreeter.config.LUSER)
-        # lang = locale.normalize(lang).replace('UTF8', 'UTF-8')
-        # logging.debug("Setting language to %s", lang)
-        # user.set_language(locale.normalize(lang))
-
-    def FinishProcess(self):
+    def close_app(self):
         """We're done, quit gtk app"""
         logging.info("Finished.")
         Gtk.main_quit()

commit 32b6c840c39b3e85fefe9cd2667443fb1b6b49b3

Translate windows when a new language is selected
This feature was broken since commit c45fc603

community-greeter.py
@@ -80,7 +80,8 @@ class CommunityGreeterApp():
         )
         self.persistence = tailsgreeter.persistence.PersistenceSettings()
         self.localisationsettings = tailsgreeter.language.LocalisationSettings(
-            usermanager_loaded_cb = self.usermanager_loaded
+            usermanager_loaded_cb = self.usermanager_loaded,
+            locale_selected_cb = self.locale_selected
         )
         self.rootaccess = tailsgreeter.rootaccess.RootAccessSettings()
         self.camouflage = tailsgreeter.camouflage.CamouflageSettings()
@@ -129,6 +130,9 @@ class CommunityGreeterApp():
         self._usermanager_ready = True
         self.localisationsettings.set_locale('en_US')
         self.maybe_show_ui()
+
+    def locale_selected(self, locale):
+        self.translate_to(locale)
 
     def maybe_show_ui(self):
         """Show UI if server and usermanager are both ready"""