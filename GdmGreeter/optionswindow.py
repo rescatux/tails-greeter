99dbdf1bfdfa889518e552f9a5259f84e2e34e8a
Tails developers <amnesia@boum.org>
2014-03-13 12:53:14 +0100

    Add a GtkScrollableWindow around the options.
    This is necessary for the options to fit low resolutions. We also make
    sure that the window is resized and positioned so that it fits in low
    resolutions without shadowing the language bar.

--- GdmGreeter/optionswindow.py
+++ GdmGreeter/optionswindow.py
@@ -45,7 +45,8 @@ class OptionsWindow(TranslatableWindow):
         self.netconf_direct = builder.get_object("netconf_direct")
         self.netconf_direct.set_active(True)
 
-        TranslatableWindow.__init__(self, builder.get_object("options_dialog"))
+        self.dialog = builder.get_object("options_dialog")
+        TranslatableWindow.__init__(self, self.dialog)
         self.window.set_visible(False)
 
         self.entry_password.set_visibility(False)
@@ -59,6 +60,30 @@ class OptionsWindow(TranslatableWindow):
         self.entry_password.connect("changed", cb_pw_changed)
         self.entry_password2.connect("changed", cb_pw_changed)
         cb_pw_changed()
+
+        ignore, langpanel_height = self.greeter.langpanel.window.get_size()
+        screen_width = self.dialog.get_screen().get_width()
+        screen_height = self.dialog.get_screen().get_height()
+        free_height = screen_height - langpanel_height
+
+        # These magic values specify a "nice" size for the options
+        # dialog. They may need to be revised, or (ideally) be set to
+        # what GTK thinks is necessary for the window to fit
+        # everything without showing a scrollbar, if that's possible.
+        nice_width = 700
+        nice_height = 700
+
+        width_request = min(nice_width, screen_width)
+        height_request = min(nice_height, free_height)
+
+        # For low (height-wise) resolution we have to un-center the
+        # dialog and move it to the top of the screen, so it won't
+        # shadow the language panel.
+        if screen_height < 768:
+            self.dialog.set_position(gtk.WIN_POS_NONE)
+            self.dialog.move((screen_width-width_request)/2, 0)
+
+        self.dialog.set_size_request(width_request, height_request)
 
     # Help callback handler
     cb_doc_handler = HelpWindow.cb_doc_handler
