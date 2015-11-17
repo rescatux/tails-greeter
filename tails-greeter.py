79bcb08e1bdd4197d346976ec3e2d6c305c87fa2
Tails developers <tails@boum.org>
2015-11-17 18:12:46 +0000

    Hide greeter windows before logging in
    Will-fix: #10563

--- tails-greeter.py
+++ tails-greeter.py
@@ -115,6 +115,8 @@ class CommunityGreeterApp():
     def login(self):
         """Login GDM to the server"""
 	logging.debug("login called")
+        for view in [self.langpanel, self.persistencewindow, self.optionswindow]:
+            view.window.hide()
         self.gdmclient.do_login(tailsgreeter.config.LUSER)
 
     def usermanager_loaded(self):
